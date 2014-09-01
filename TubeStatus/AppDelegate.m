f189d2b7a1445004cfd662b761b702dd00b4dd96
Dylan Maryk <dylan@dylanmaryk.com>
2014-09-01 11:33:24 +0100

    Implement registering for and receiving push notifications.

--- TubeStatus/AppDelegate.m
+++ TubeStatus/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "DataModel.h"
+#import "AFHTTPRequestOperationManager.h"
 
 @interface AppDelegate ()
 
@@ -15,30 +17,36 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+    
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSString *deviceTokenString = [self deviceTokenStringFromData:deviceToken];
+    
+    if (![deviceTokenString isEqualToString:[[DataModel getUserDefaults] valueForKey:@"deviceToken"]]) {
+        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+        [manager POST:@"http://api.tubestatus.dylanmaryk.com/tokens" parameters:@{ @"devicetoken": deviceTokenString } success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            [DataModel setUserDefaultsObject:deviceTokenString forKey:@"deviceToken"];
+        } failure:nil];
+    }
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    completionHandler(UIBackgroundFetchResultNoData);
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+- (NSString *)deviceTokenStringFromData:(NSData *)deviceToken {
+    const char *deviceTokenData = deviceToken.bytes;
+    
+    NSMutableString *deviceTokenString = [NSMutableString string];
+    
+    for (int i = 0; i < deviceToken.length; i++) {
+        [deviceTokenString appendFormat:@"%02.2hhX", deviceTokenData[i]];
+    }
+    
+    return deviceTokenString.copy;
 }
 
 @end
